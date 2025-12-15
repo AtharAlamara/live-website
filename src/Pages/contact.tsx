99a65ccc329d19b153888ce86ece8f7b2f4d699e
AtharAlamara <ceo@atharalamara.sa>
2025-12-16 00:43:21 +0200

    Updated contact.tsx

--- src/Pages/contact.tsx
+++ src/Pages/contact.tsx
@@ -295,11 +295,11 @@ function ContactPage() {
                   <div className="flex items-start gap-2">
                     <span className="text-[#000000] text-xl">•</span>
                     <p
-                      className="text-lg font-thin"
-                      style={{
-                        fontWeight: !isAr && isWindows ? 100 : undefined,
-                      }}
-                    >
+        className="text-lg font-thin"
+        style={{
+          opacity: isWinEn ? 0.65 : 1,
+        }}
+      >
                       {t('{hours-value}', 'Sun–Thu, 9:00–17:00')}
                     </p>
                   </div>
