24fc2532740e48804bc19996a0d1781049b7d345
AtharAlamara <ceo@atharalamara.sa>
2025-12-31 13:13:31 +0200

    Updated ProjectTemplate.tsx

--- src/Components/ProjectTemplate.tsx
+++ src/Components/ProjectTemplate.tsx
@@ -103,8 +103,7 @@ export default function ProjectTemplate({
           {/* TOP TEXT BLOCK */}
           <div className="text-center mb-8">
 
-            const force001VillaBidi = isAr && projectSlug === "/projects/001-villa";
-...
+
 <h1 className="text-xl font-bold">
   <span
     dir={force001VillaBidi ? "ltr" : undefined}
