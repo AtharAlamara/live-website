6f759903b8b32e226b68903b7716cee39e7edf0e
AtharAlamara <ceo@atharalamara.sa>
2026-01-07 23:13:03 +0200

    Updated article-new-murabba-tranformation.tsx

--- src/Pages/article-new-murabba-tranformation.tsx
+++ src/Pages/article-new-murabba-tranformation.tsx
@@ -297,7 +297,7 @@ export default function Article7() {
           relatedArticles={{
             title: isAr ? 'من مذكّراتنا' : 'More from our Journal',
             description: isAr
-              ? 'اكتشف مقالات ورؤى إضافية من أثر العمارة.'
+              ? 'اكتشف مقالات أخرى من أثر العمارة'
               : 'Explore additional insights and stories from Athar Architecture.',
             items: relatedArticles,
           }}
