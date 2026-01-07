58c84fc380467d9f6232e4d9fce7043ec49f5679
AtharAlamara <ceo@atharalamara.sa>
2026-01-07 23:12:49 +0200

    Updated article-red-sea-global-leed-achievements.tsx

--- src/Pages/article-red-sea-global-leed-achievements.tsx
+++ src/Pages/article-red-sea-global-leed-achievements.tsx
@@ -279,7 +279,7 @@ export default function Article6() {
           relatedArticles={{
             title: isAr ? 'من مذكّراتنا' : 'More from our Journal',
             description: isAr
-              ? 'اكتشف مقالات ورؤى إضافية من أثر العمارة.'
+              ? 'اكتشف مقالات أخرى من أثر العمارة'
               : 'Explore additional insights and stories from Athar Architecture.',
             items: relatedArticles,
           }}
